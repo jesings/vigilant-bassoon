b5bf561bfdc05e9d7e37e3ae872a189892cafc4e
jesings <jsinger@stuy.edu>
2018-10-23 14:29:02 -0400

    Bassoon made ever more vigilant

--- Downloads/Systems/fileiotest/testreadwrite.c
+++ Downloads/Systems/fileiotest/testreadwrite.c
@@ -3,19 +3,20 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 int main(){
     int myfile = open("testreadwrite.c",O_RDONLY);
     char* mine = malloc(sizeof(char)*1000);
-    fread(myfile,mine,500);
+    read(myfile,mine,1000);
     close(myfile);
     puts(mine);
-   // char* nice = "file .c"
-   // for(char i = 0;i<256;i++){
-   //     char filename[10];
-   //     strcpy(nice, filename);
-   //     filename[4] = i;
-   //     FILE* copy = fopen(filename,"w");
-   //     fwrite(
-   // }
+    for(unsigned char i = 0;i<255;i++){
+        char filename[10];
+        strcpy(filename,"file .c");
+        filename[4] = i;
+        int copy = open(filename,O_WRONLY | O_CREAT,0644);
+        write(copy,mine,1000);
+        close(copy);
+    }
     return 0;
 }
